f032496b91c85447d8b33983669f1d2d185b1f7f
AndyOximenes <andy.oximenes@gmail.com>
2020-02-25 18:52:08 -0300

    Setting up the Mailchimp API

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const request = require("request")
+const app = express();
 
-const app = express()
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/signup.html")
-})
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
 
-    var firstName = req.body.firstName
-    var lastName = req.body.lastName
-    var email = req.body.email
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-    console.log(firstName, lastName, email);
-    
+  const jsonData = JSON.stringify(data);
 
-})
+  const url = "https://us4.api.mailchimp.com/3.0/lists/07aca0acb5";
 
+  const options = {
+    method: "POST",
+    auth: "andy:dc5e71ec91447784c03ca7d9ff41005b-us4"
+  };
 
+  const request = https.request(url, options, response => {
+    response.on("data", data => {
+      console.log(JSON.parse(data));
+    });
+  });
 
-
-
-
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-    
-})
+  console.log("Server is running on port 3000");
+});
+
+// API Key
+// dc5e71ec91447784c03ca7d9ff41005b-us4
+
+// List ID
+// 07aca0acb5

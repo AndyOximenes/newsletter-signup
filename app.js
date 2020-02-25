f9c3dd9eeebe3b11b3073f85353499ac5b26e92c
AndyOximenes <andy.oximenes@gmail.com>
2020-02-25 18:01:36 -0300

    Setting up the sign up page and getting the data that's inside the inputs

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const request = require("request")
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", (req, res) => {
+
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    var email = req.body.email
+
+    console.log(firstName, lastName, email);
+    
+
+})
 
 
 
